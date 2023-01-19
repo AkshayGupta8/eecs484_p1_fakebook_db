1d90d5f4d10bbe4790c4a08746c1a47288c694ec
AkshayGupta8 <akshay.g@icloud.com>
2023-01-19 13:42:50 -0500

    added base tables --> need to add attributes

---
+++ createTables.sql
@@ -0,0 +1,112 @@
+CREATE TABLE Users(
+    user_id INTEGER NOT NULL,
+    first_name VARCHAR2(100) NOT NULL,
+    last_name VARCHAR2(100) NOT NULL,
+    year_of_birth INTEGER,
+    month_of_birth INTEGER,
+    day_of_birth INTEGER,
+    gender VARCHAR2(100)
+);
+
+
+CREATE TABLE Friends(
+    user1_id INTEGER NOT NULL
+    user2_id INTEGER NOT NULL
+    -- TODO implement friends trigger. See spec
+);
+
+CREATE TABLE Cities(
+    city_id INTEGER NOT NULL,
+    city_name VARCHAR2(100) NOT NULL,
+    state_name VARCHAR2(100) NOT NULL,
+    country_name VARCHAR2(100) NOT NULL,
+);
+
+CREATE TABLE User_Current_Cities(
+    user_id INTEGER NOT NULL,
+    hometown_city_id INTEGER NOT NULL
+);
+
+
+CREATE TABLE User_Hometown_Cities(
+    user_id INTEGER NOT NULL
+    hometown_city_id INTEGER NOT NULL
+);
+
+
+CREATE TABLE Messages(
+    message_id INTEGER NOT NULL,
+    sender_id INTEGER NOT NULL,
+    receiver_id INTEGER NOT NULL,
+    message_content VARCHAR2(2000) NOT NULL,
+    sent_time TIMESTAMP NOT NULL
+);
+
+CREATE TABLE Programs(
+    program_id INTEGER NOT NULL,
+    institution VARCHAR2(100) NOT NULL,
+    concentration VARCHAR2(100) NOT NULL,
+    degree VARCHAR2(100) NOT NULL
+);
+
+CREATE TABLE 
+
+CREATE TABLE Education(
+    user_id INTEGER NOT NULL,
+    program_id INTEGER NOT NULL,
+    program_year INTEGER NOT NULL
+);
+
+CREATE TABLE User_Events(
+    event_id INTEGER NOT NULL,
+    event_creator_id INTEGER NOT NULL,
+    event_name VARCHAR2(100) NOT NULL,
+    event_tagline VARCHAR2(100),
+    event_description VARCHAR2(100),
+    event_host VARCHAR2(100),
+    event_type VARCHAR2(100),
+    event_subtype VARCHAR2(100),
+    event_address VARCHAR2(2000),
+    event_city_id INTEGER NOT NULL,
+    event_start_time TIMESTAMP,
+    event_end_time TIMESTAMP
+);
+
+CREATE TABLE Participants(
+    event_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    confirmation VARCHAR2(100) NOT NULL
+    -- TODO: confirmation must be either 
+        -- Attending
+        -- Unsure
+        -- Declines
+        -- Not_Replied
+);
+
+CREATE TABLE Albums(
+    album_id INTEGER NOT NULL,
+    album_owner_id INTEGER NOT NULL,
+    album_name VARCHAR2(100) NOT NULL,
+    album_created_time TIMESTAMP NOT NULL,
+    album_modified_time TIMESTAMP,
+    album_link VARCHAR(2000) NOT NULL,
+    album_visibility VARCHAR(100) NOT NULL,
+    cover_photo_id INTEGER NOT NULL
+);
+
+CREATE TABLE Photos (
+    photo_id INTEGER NOT NULL,
+    album_id INTEGER NOT NULL,
+    photo_caption VARCHAR2(2000),
+    photo_created_time TIMESTAMP NOT NULL,
+    photo_modified_time TIMESTAMP,
+    photo_link VARCHAR2(2000) NOT NULL
+);
+
+CREATE TABLE Tags (
+    tag_photo_id INTEGER NOT NULL,
+    tag_subject_id INTEGER NOT NULL,
+    tag_created_time TIMESTAMP NOT NULL,
+    tag_x NUMBER NOT NULL,
+    tag_y NUMBER REQUIRED
+);
