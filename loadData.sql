b3e3197d44824006a3055993bec920afeee88f0c
Akshay Gupta <akshay.g@icloud.com>
2023-01-23 00:26:07 -0500

    user_current_cities

--- loadData.sql
+++ loadData.sql
@@ -28,3 +28,9 @@ INSERT INTO Friends(user1_id, user2_id) SELECT user1_id, user2_id FROM project1.
 -- TODO: Couldn't figure out what to do with the city_id field
 -- INSERT INTO Cities(city_id, city_name, state_name, country_name) 
 -- SELECT DISTINCT event_city, event_state, event_country FROM project1.Public_Event_Information
+
+-- User_Current_Cities
+    -- user_id INTEGER NOT NULL,
+    -- current_city_id INTEGER NOT NULL,
+-- TODO: Not sure how to do this yet, need to figure out inner join as well as city_id for cities
+
