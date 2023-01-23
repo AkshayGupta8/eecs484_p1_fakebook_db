b758066785fd5b8db1db3cf751eb9fcd99743314
Akshay Gupta <akshay.g@icloud.com>
2023-01-23 12:26:32 -0500

    Education done

--- loadData.sql
+++ loadData.sql
@@ -72,11 +72,59 @@ AND program_degree IS NOT NULL;
     user_id INTEGER NOT NULL,
     program_id INTEGER NOT NULL,
     program_year INTEGER NOT NULL,
-TODO: Figure out how iintersect works
-INSERT INTO Education(user_id, institution, program_year)
-SELECT DISTINCT */
+TODO: why 1646 rows? too much? */
 INSERT INTO Education
-SELECT DISTINCT user_id, city_id
-FROM project1.Public_User_Information p
-INNER JOIN Cities c
-ON p.hometown_city = c.city_name AND p.hometown_state = c.state_name AND p.hometown_country = c.country_name;
+SELECT DISTINCT user_id, program_id, program_year
+FROM project1.Public_User_Information pub
+INNER JOIN Programs prog
+ON pub.institution_name = prog.institution 
+AND pub.program_degree = prog.degree 
+AND pub.program_concentration = prog.concentration;
+
+/* CREATE TABLE User_Events(
+    event_id INTEGER NOT NULL,
+    event_creator_id INTEGER NOT NULL,
+    event_name VARCHAR2(100) NOT NULL,
+    event_tagline VARCHAR2(100),
+    event_description VARCHAR2(100),
+    event_host VARCHAR2(100),
+    event_type VARCHAR2(100),
+    event_subtype VARCHAR2(100),
+    event_address VARCHAR2(2000),
+    event_city_id INTEGER NOT NULL,
+    event_start_time TIMESTAMP,
+    event_end_time TIMESTAMP,
+    PRIMARY KEY (event_id),
+    FOREIGN KEY (event_creator_id) REFERENCES Users(user_id)
+);
+*/
+
+/* Participants
+No public data for participants. */
+
+/* CREATE TABLE Albums(
+    album_id INTEGER NOT NULL,
+    album_owner_id INTEGER NOT NULL,
+    album_name VARCHAR2(100) NOT NULL,
+    album_created_time TIMESTAMP NOT NULL,
+    album_modified_time TIMESTAMP,
+    album_link VARCHAR2(2000) NOT NULL,
+    album_visibility VARCHAR2(100) NOT NULL,
+    cover_photo_id INTEGER NOT NULL,
+    PRIMARY KEY (album_id),
+    FOREIGN KEY (album_owner_id) REFERENCES Users(user_id),
+    CHECK (album_visibility in ('Everyone','Friends','Friends_Of_Friends', 'Myself'))
+);
+
+*/
+
+/* CREATE TABLE Photos(
+    photo_id INTEGER NOT NULL,
+    album_id INTEGER NOT NULL,
+    photo_caption VARCHAR2(2000),
+    photo_created_time TIMESTAMP NOT NULL,
+    photo_modified_time TIMESTAMP,
+    photo_link VARCHAR2(2000) NOT NULL,
+    PRIMARY KEY (photo_id)
+);
+*/
